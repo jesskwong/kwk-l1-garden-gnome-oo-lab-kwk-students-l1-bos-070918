bfb854142de0c88ef6120f66c98157e7673dbcb3
jesskwong <jesskwong9@gmail.com>
2018-07-13 18:08:42 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -3,3 +3,14 @@ class GardenGnome
   attr_accessor :name,:age,:gluten_allergy
   attr_reader :personality,:hat_color 
   
+  def initialize(hat_color = "red")
+    @personality = "evil"
+    @hat_color = hat_color 
+    
+  end        
+  
+  def gnaw 
+    
+  def shout 
+      
+  def introduce_self
